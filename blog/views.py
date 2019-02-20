29dbc2f1600b81902cf6722e16279975d9f9441e
E-Designs <34104219+E-Designs@users.noreply.github.com>
2019-02-19 17:48:57 -0800

    Remove dummy data

--- blog/views.py
+++ blog/views.py
@@ -3,22 +3,7 @@ from django.http import HttpResponse
 from .models import Post
 
 # Create your views here.
-#dummy data fake posts.
-posts = [
-    {
-       'author': 'ErnestLH',
-       'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-#end fake posts
+
 def home(reqest):
     context = {
         'posts': Post.objects.all()
